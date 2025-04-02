2d9512f69024b7994afd15d90522368cd0936d48
epicemrys <emrysweb@outlook.comm>
2025-04-02 14:07:31 +0100

    Fix Swagger API

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const passport = require('passport');
+const swaggerRouter = require('./swagger');
 
-router.use('/', require('./swagger'));
+router.use('/', swaggerRouter);
 
 router.use('/lovebox', require('./lovebox'));
 router.use('/connectbox', require('./connectbox'));
--- routes/swagger.js
+++ routes/swagger.js
@@ -1,6 +1,7 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
+const swaggerDocument = require('./swagger.json');
 
 // Serve Swagger UI at /api-docs
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
