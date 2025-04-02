cae4e47cc1a41a3b17121ac318f6a0efb443a5eb
epicemrys <emrysweb@outlook.comm>
2025-04-02 11:37:31 +0100

    Render update API

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
-const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const passport = require('passport');
 const session = require('express-session');
 const GitHubStrategy = require('passport-github2').Strategy;
-const app = express();
-
+const cors = require('cors');
 
 const port = process.env.PORT || 9090;
+const app = express();
 
 
 app.use(express.json());
 app.use(bodyParser.json())
-app.use(session({secret: "secret", resolve: false, resave: false, saveUninitialized: true, store: new express-session.MemoryStore()}));
+app.use(session({ secret: "secret", resolve: false, resave: false, saveUninitialized: true, store: new express - session.MemoryStore() }));
 
-// app.use(cors({
-//     origin: '*',
-//     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-//     credentials: true
-// }));
 
-app.use('/', require('./routes/index.js'));
-
+app.use(passport.initialize())
+app.use(passport.session())
 app.use((req, res, next) => {
     res.setHeader('Access-control-Allow-Origin', '*');
     res.setHeader(
@@ -33,10 +27,17 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+}));
+
+app.use('/', require('./routes/index.js'));
+
 
 process.on('uncaughtException', (err, origin) => {
     console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
-  });
+});
 
 
 
@@ -45,10 +46,10 @@ passport.use(new GitHubStrategy({
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: process.env.CALLBACK_URL
 },
-function(accessToken, refreshToken, profile, done) {
-    console.log(profile);
-    return done(null, profile);
-}
+    function (accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        return done(null, profile);
+    }
 ));
 
 passport.serializeUser((user, done) => {
@@ -72,10 +73,10 @@ app.get('/', (req, res) => {
 });
 
 mongodb.initDb((err) => {
-    if(err) {
+    if (err) {
         console.log(err);
     }
     else {
-        app.listen(port, () => {console.log(`Database = Active | node Running on port ${port}`)});
+        app.listen(port, () => { console.log(`Database = Active | node Running on port ${port}`) });
     }
 });
