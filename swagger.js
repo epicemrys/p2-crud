bddcd930fc1936b613c0f68c55f8019625bbac97
epicemrys <emrysweb@outlook.comm>
2025-04-03 15:19:23 +0100

    Changing url

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         title: 'Match Maker Api',
         description: 'Connect and Match Api',
     },
-    host: 'p2-crud.onrender.com',
+    host: 'localhost:9091',
     schemes: ['https', 'http']
 };
 
